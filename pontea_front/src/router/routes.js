25cf45afcaa2d8ffec459aab06f1c1da1a3cc16b
Layse Araújo <layseraraujo@gmail.com>
2023-09-27 08:16:32 -0300

    rotas

--- pontea_front/src/router/routes.js
+++ pontea_front/src/router/routes.js
@@ -1,4 +1,3 @@
-
 const routes = [
   {
     path: '/',
@@ -10,31 +9,53 @@ const routes = [
         next('/login');
       }
     },
-    redirect: '/login',
+    redirect: '/home',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '/home', component: () => import('pages/HomePage.vue') },
-      { path: '/carteira', component: () => import('pages/WalletPage.vue') },
-      { path: '/atividades', component: () => import('pages/ActivitiesPage.vue'), // Rota pai "atividades"
+      { path: 'home', component: () => import('pages/HomePage.vue') },
+      { path: 'carteira', component: () => import('pages/WalletPage.vue') },
+      { path: 'atividades', component: () => import('pages/ActivitiesPage.vue'),
         children: [
-          { path: 'detalhes', component: () => import('pages/DetailsPage.vue') } // Rota filha "detalhes" -> atividades/detalhes
+          { path: 'detalhes', component: () => import('pages/DetailsPage.vue') } 
         ]
       },
-      { path: '/experiencias', component: () => import('pages/ExperiencesPage.vue') },
-      { path: '/educadores', component: () => import('pages/EducatorsPage.vue') },
-      { path: '/busca', component: () => import('pages/SearchPage.vue') },
-      { path: '/perfilEducador', component: () => import('pages/PerfilEducador.vue') },
-      { path: '/perfilCliente', component: () => import('pages/PerfilCliente.vue') },
-      { path: '/ser-educador', component: () => import('pages/SerEducadorPage.vue') },
-      { path: '/adicionar-atividade', component: () => import('pages/AddAtividadePage.vue') },
+      { path: 'experiencias', component: () => import('pages/ExperiencesPage.vue') },
+      { path: 'educadores', component: () => import('pages/EducatorsPage.vue') },
+      { path: 'busca', component: () => import('pages/SearchPage.vue') },
+      { path: 'perfilEducador', component: () => import('pages/PerfilEducador.vue') },
+      { path: 'perfilCliente', component: () => import('pages/PerfilCliente.vue') },
+      { path: 'ser-educador', component: () => import('pages/SerEducadorPage.vue') },
+      { path: 'adicionar-atividade', component: () => import('pages/AddAtividadePage.vue') },
     ]
   },
   {
-    path: '/',
+    path: '/login',
+    beforeEnter: (to, from, next) => {
+      const token = localStorage.getItem("token");
+      if (token) {
+        next('/home');
+      } else {
+        next();
+      }
+    },
     component: () => import('layouts/LandingLayout.vue'),
     children: [
-      { path: '/login', component: () => import('pages/LoginPage.vue') }, 
-      { path: '/cadastro', component: () => import('pages/CadastroPage.vue') },
+      { path: '', component: () => import('pages/LoginPage.vue') },
+    ]
+  },
+  {
+    path: '/cadastro',
+    beforeEnter: (to, from, next) => {
+      const token = localStorage.getItem("token");
+      if (token) {
+        next('/home');
+      } else {
+        next();
+      }
+    },
+    component: () => import('layouts/LandingLayout.vue'),
+    children: [
+      { path: '', component: () => import('pages/CadastroPage.vue') },
     ]
   },
   {
