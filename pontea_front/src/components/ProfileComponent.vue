7e04b61b2a95e6cab561306a182060130c67b40e
Gabriela <50087532+GabrielaSouza2@users.noreply.github.com>
2023-09-29 16:21:23 -0300

    feat: wallet and transfer completed

---
+++ pontea_front/src/components/ProfileComponent.vue
@@ -0,0 +1,176 @@
+<template>
+  <div class="profile">
+    <img
+      src="https://i.imgur.com/wJN5wJx.png"
+      alt="Imagem"
+      class="rounded-image"
+    />
+
+    <div class="name flex">
+      <h2 class="title">Amanda Borba</h2>
+      <a href="#" class="icon-button">
+        <img src="/../icons/edit.svg" alt="Edit Profile" />
+      </a>
+    </div>
+
+    <div class="rating flex">
+      <div class="stars">
+        <span>4,1</span>
+        <img src="/../icons/star.svg" alt="Star" />
+      </div>
+
+      <span>552 avaliações</span>
+    </div>
+
+    <div class="wallet flex">
+      <img src="/../icons/wallet.svg" alt="Edit Profile" />
+      <p>R$ 62,16</p>
+    </div>
+
+    <div class="description">
+      <p>
+        Professora na Universidade Federal de Pernambuco, pós-graduada em
+        educação inclusiva para crianças neuroatípicas pela UNINASSAU e
+        licenciada em Pedagogia pela Universidade do Minho.
+      </p>
+    </div>
+    <q-btn
+      class="q-px-xl q-py-sm rounded"
+      label="Ser educador"
+      color="primary"
+      @click="changePage(showHistory)"
+      v-if="showBtn"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ProfileComponent",
+  props: {
+    showBtn: Boolean,
+  },
+  components: {},
+  data() {
+    return {
+      showHistory: true,
+      title: "Minha carteira",
+    };
+  },
+  methods: {},
+};
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  line-height: auto !important;
+}
+
+p,
+h2 {
+  margin: 0;
+  line-height: auto !important;
+}
+
+.profile {
+  max-width: 320px;
+  padding: 40px 32px;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  position: fixed;
+  gap: 16px;
+
+  height: 100%;
+}
+
+.flex {
+  display: flex;
+  align-items: center;
+  gap: 8px;
+}
+
+.rounded {
+  border-radius: 20px;
+  position: fixed;
+  bottom: 100px;
+}
+
+.rounded-image {
+  border-radius: 100%;
+  width: 104px;
+  height: 104px;
+}
+
+.title {
+  color: #3c3c3f;
+  font-size: 16px;
+  font-weight: 500;
+  line-height: 0 !important;
+  font-family: Lexend;
+}
+
+.icon-button {
+  width: 16px;
+  height: 16px;
+}
+
+.rating {
+  font-size: 12.8px;
+  font-family: Lexend;
+  font-weight: 300;
+}
+
+.stars span {
+  margin-right: 3px;
+}
+
+.scrollable-div {
+  max-height: calc(100vh - 410px);
+  overflow-y: scroll;
+  padding-right: 10px;
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+}
+
+.scrollable-div::-webkit-scrollbar {
+  width: 8px;
+}
+
+.scrollable-div::-webkit-scrollbar-thumb {
+  background-color: #888;
+  border-radius: 6px;
+}
+
+.scrollable-div::-webkit-scrollbar-thumb:hover {
+  background-color: #555;
+}
+
+.scrollable-div::-webkit-scrollbar-track {
+  background-color: #f0f0f0;
+  border-radius: 6px;
+}
+
+.wallet {
+  background-color: #dce9f5;
+  padding: 4px 8px;
+  border-radius: 4px;
+}
+
+.wallet p {
+  font-family: Lexend;
+  color: #144ec0;
+  font-weight: 500;
+  font-size: 12.8px;
+}
+
+.description {
+  font-family: Lexend;
+  font-weight: 300;
+}
+</style>
--- pontea_front/src/pages/WalletPage.vue
+++ pontea_front/src/pages/WalletPage.vue
@@ -1,253 +1,173 @@
 <template>
-  <div>
-    <div class="container">
-      <div class="box box1">
-        <div class="sub-box">
-          <img src="https://evorastudio.com.br/wp-content/uploads/2021/04/retrato-corporativo-foto-perfil-profissional-foto-linkedin-24-scaled.jpg" alt="Imagem" class="rounded-image">
+  <div class="container">
+    <ProfileComponent :showBtn="false" />
+
+    <div class="historic">
+      <h3 class="title">{{ title }}</h3>
+
+      <section class="flex wallet-info">
+        <div class="flex">
+          <p class="label">Saldo disponível:</p>
+          <span class="available-balance info">R$ 62,16</span>
         </div>
-        <div class="sub-box" id="nome"><p>Nome do perfil</p></div>
-        <div class="sub-box">estrela e avaliação</div>
-        <div class="sub-box" id="descricao"><p>Professora na Universidade Federal de Pernambuco,
-           pós-graduada em educação inclusiva para crianças neuroatípicas pela UNINASSAU e licenciada
-            em Pedagogia pela Universidade do Minho.</p></div>
-        <div class="sub-box"><q-btn label="Ser educador" color="primary" no-caps rounded /></div>
-      </div>
-      <div class="box box2">
-        <div><h5 class="titulo">Minha carteira</h5></div>
-        <div class="paragraphs">
-          <h5 class="paragraph" id="saldoD">Saldo disponível:</h5>
-          <p class="paragraph" id="valorD">R$ 62,35</p>
-          <h5 class="paragraph" id="saldoD">Valor pago a PONTEA:</h5>
-          <p class="paragraph" id="valorP">R$ 43,45</p>
-          <h5 class="paragraph" id="saldoD">Conta do educador:</h5>
-          <p class="paragraph" id="status">Verificado</p>
+        <div class="flex">
+          <p class="label">Valor pago a Pontea:</p>
+          <span class="amount-paid info">R$ 26,64</span>
         </div>
-      <div></div>
+        <div class="flex">
+          <p class="label">Conta do Educador:</p>
+          <span class="educator-account info">Verificada</span>
+        </div>
+      </section>
+
+      <h3 class="title" v-if="showHistory">Histórico de vendas</h3>
+      <div class="scrollable-div" v-if="showHistory">
+        <BalanceComponent />
+        <TransactionCard />
+        <TransactionCard />
+        <BalanceComponent />
+        <TransactionCard />
+        <TransactionCard />
+        <TransactionCard />
+        <TransactionCard />
+        <TransactionCard />
+        <TransactionCard />
+        <TransactionCard />
+        <TransactionCard />
+        <TransactionCard />
+        <TransactionCard />
+        <TransactionCard />
+      </div>
+      <div v-else>
+        <TransferComponent />
       </div>
     </div>
-  </div>
-</template>
 
-<style>
-.container {
-  display: flex;
-  justify-content: space-between;
-}
-
-.paragraphs {
-  white-space: nowrap; /* Impede que os parágrafos quebrem para a próxima linha */
-}
-
-#valorP{
-  color: var(--textos-cinza, #616167);
-  text-align: center;
-  font-family: Lexend;
-  font-size: 16px;
-  font-style: normal;
-  font-weight: 500;
-  line-height: 24px; /* 150% */
-  margin-left: -0.75%;
-  margin-right: 15%;
-}
-
-#valorD{
-  color: var(--verde, #65A115);
-  text-align: center;
-  font-family: Lexend;
-  font-size: 16px;
-  font-style: normal;
-  font-weight: 500;
-  line-height: 24px; 
-  margin-left: -0.75%;
-  margin-right: 15%;
-}
-
-#saldoD{
-  color: var(--Azul, #144EC0);
-  text-align: center;
-  font-family: Lexend;
-  font-size: 12.8px;
-  font-style: normal;
-  font-weight: 300;
-  line-height: 16px; /* 125% */
-}
-
-#status{
-  color: var(--Azul, #144EC0);
-  text-align: center;
-  font-family: Lexend;
-  font-size: 12.8px;
-  font-style: normal;
-  font-weight: 500;
-  margin-left: -0.75%;
-  line-height: 16px; /* 125% */
-}
-
-.paragraph {
-  display: inline-block;
-  margin: 0 10px; /* Adicione margem horizontal entre os parágrafos para espaçamento */
-}
-
-.box {
-  gap: 40px;
-}
-
-.box1 {
-  flex: 1;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  align-items: center;
-  padding-left: 2%;
-}
-
-.box2 {
-  flex: 3;
-  display: flex;
-  flex-direction: column;
-}
-
-.sub-box {
-  margin: 5px 0;
-  display: flex;
-  align-items: center; 
-  justify-content: center; 
-}
-
-
-.rounded-image {
-  margin-top: 10%;
-  border-radius: 50%; 
-  width: 100px; 
-  height: 100px; 
-  width: 180px; 
-  height: 180px; 
-}
-
-.titulo{
-  color: var(--textos-cinza, #616167);
-  font-family: Lexend;
-  font-size: 16px;
-  font-style: normal;
-  font-weight: 500;
-  line-height: 24px; /* 150% */
-}
-
-#nome {
-  color: var(--textos-preto, #3C3C3F);
-  font-family: Lexend;
-  font-size: 16px;
-  font-style: normal;
-  font-weight: 500;
-  line-height: 24px; /* 150% */
-}
-
-#descricao{
-  align-self: stretch;
-  color: var(--textos-preto, #3C3C3F);
-  /* PARAGRAPH 2 */
-  font-family: Lexend;
-  font-size: 12.8px;
-  font-style: normal;
-  font-weight: 300;
-  line-height: 16px; /* 125% */
-}
-
-
-
-</style>
-
-<!-- 
-
-
-
-
-
-
-
-<template>
-  <div class="center">
-    <div
-      id="titulo"
-      :style="{
-        styleName: 'H5',
-        fontFamily: 'Lexend',
-        fontSize: '16px',
-        fontWeight: '500',
-        lineHeight: '24px',
-        letterSpacing: '0em',
-        textAlign: 'center',
-        fontVariationSettings: '\'slnt\' 0',
-        width: '116px',
-        height: '24px',
-        color: 'var(--textos-preto, #3C3C3F)', 
-      }"
-    >
-      Minha carteira
-    </div>
-
- Conteúdo "Saldo" 
-    <div
-      id="saldo"
-      :style="{
-        fontFamily: 'Lexend',
-        fontSize: '12.8px',
-        fontStyle: 'normal',
-        fontWeight: '300',
-        lineHeight: '16px',
-        letterSpacing: '0em',
-        textAlign: 'left',
-        color: 'var(--Azul, #144EC0)',
-        marginTop: '16px' 
-      }"
-    >
-      Saldo
-    </div>
-
-    Conteúdo "R$ 62,16" 
-    <div
-      id="valor"
-      :style="{
-        fontFamily: 'Lexend',
-        fontSize: '16px',
-        fontStyle: 'normal',
-        fontWeight: '500',
-        lineHeight: '24px',
-        letterSpacing: '0em',
-        textAlign: 'left',
-        color: 'var(--verde, #65A115)',
-      }"
-    >
-      R$ 62,16
+    <div class="bottom" v-if="showHistory">
+      <q-btn
+        class="q-px-xl q-py-sm rounded"
+        label="Resgatar saldo"
+        type="submit"
+        color="primary"
+        @click="changePage(showHistory)"
+      />
     </div>
   </div>
 </template>
 
 <script>
+import ProfileComponent from "src/components/ProfileComponent.vue";
+import TransactionCard from "src/components/TransactionCard.vue";
+import BalanceComponent from "src/components/BalanceComponent.vue";
+import TransferComponent from "src/components/TransferComponent.vue";
+
 export default {
+  name: "WalletPage",
+  components: {
+    ProfileComponent,
+    TransactionCard,
+    BalanceComponent,
+    TransferComponent,
+  },
   data() {
     return {
-      title: 'Minha carteira',
+      showHistory: true,
+      title: "Minha carteira",
     };
+  },
+  methods: {
+    changePage(value) {
+      if (value) {
+        this.title = "Transferência";
+      } else {
+        this.title = "Minha carteira";
+      }
+      this.showHistory = !value;
+    },
   },
 };
 </script>
 
-
-
 <style>
-.center-container {
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  line-height: auto !important;
+}
+
+p,
+h2 {
+  margin: 0;
+  line-height: auto !important;
+}
+
+.container {
+  display: flex;
+  flex-direction: row;
+}
+
+.flex {
+  display: flex;
+  align-items: center;
+  gap: 8px;
+}
+
+.historic {
+  width: 100%;
   display: flex;
   flex-direction: column;
-  align-items: flex-start;
-  gap: 16px;
-  align-self: stretch;
+  gap: 20px;
+  padding: 30px;
+  margin-left: 320px;
 }
 
-.center{
+.wallet-info {
   display: flex;
+  justify-content: space-between;
 }
-.font-size {
-  /* Estilos para o elemento centralizado, como font-size, font-family, etc. */
+
+.label {
+  font-family: Lexend;
+  font-weight: 300;
+  font-size: 12.8px;
+  color: #144ec0;
 }
-</style> -->
+
+.info {
+  font-size: 16px;
+  font-weight: 500;
+}
+
+.available-balance {
+  color: #65a115;
+}
+
+.amount-paid {
+  color: #616167;
+}
+
+.educator-account {
+  color: #144ec0;
+  font-size: 12.8px;
+}
+
+.bottom {
+  background-color: white;
+  width: 100%;
+  height: 150px;
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  box-shadow: 0px -4px 20px 0px rgba(0, 0, 0, 0.1);
+  display: flex;
+  align-items: flex-start;
+  justify-content: center;
+  z-index: 1;
+}
+
+.rounded {
+  border-radius: 20px;
+  margin-top: 16px;
+}
+</style>
